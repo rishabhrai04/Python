2a45b8f28e3bceb8eff35e63f009f2842dcca370
Rishabh Rai <rishabhrai969@gmail.com>
2025-04-11 23:15:41 +0530

    Initial commit
    practice set 6.

---
+++ Chapter6_Practice_Set/05_problem5.py
@@ -0,0 +1,9 @@
+# Write a program which find out whether a given name is present in a list or not.
+
+l = ["Ali", "Tariq", "Ahmed", "Sara", "Aisha"]
+name = input("Enter your name: ")
+if name in l:
+    print("Your name is present in the list")   
+
+else:   
+    print("Your name is not present in the list")
---
+++ Chapter6_Practice_Set/07_problem7.py
@@ -0,0 +1,7 @@
+# Write a program to find out whether a given post is talking about "Harry"or not.
+
+post = input("Enter your post: ")
+if ("Harry".lower() in post.lower()):
+    print("This post is talking about Harry")
+else:
+    print("This post is not talking about Harry")   
