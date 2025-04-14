facef9a5b90e15378f3da9dffad8f2d685e81f88
Rishabh Rai <rishabhrai969@gmail.com>
2025-04-15 00:15:50 +0530

    Add files via upload
    File I/O: Types of Files; Modes of Opening a file.

---
+++ Chapter9/01_file.py
@@ -0,0 +1,10 @@
+'''
+a= "a very long string with emails "
+
+emails = []
+3 seconds'''
+
+f= open("file.text") # Open is a built in function in python.
+data = f.read()      # f.read is used to read the file .
+print(data) 
+f.close()
---
+++ Chapter9/04_append.py
@@ -0,0 +1,7 @@
+st = "Hey Rishabh you are amazing"
+
+f = open("myfile.txt","a")
+
+f.write(st)
+
+f.close()
